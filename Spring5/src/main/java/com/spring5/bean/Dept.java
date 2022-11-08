b37723fe21f3bbdcb107b267542d9abfea8dfe36
jason_glb <shiyujia@kean.edu>
2022-11-08 02:55:12 -0500

    d2

---
+++ Spring5/src/main/java/com/spring5/bean/Dept.java
@@ -0,0 +1,17 @@
+package com.spring5.bean;
+
+/**
+ * @Author: Jason
+ * @Date: 2022/11/7 13:17 13 17
+ */
+public class Dept {
+
+    private String dName;
+
+    public String getdName() {
+        return dName;
+    }
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+}
